4f68cf2a7c26b564fa74060fac9f0de492452c46
rpal123 <rstgopal@gmail.com>
2026-01-03 06:51:28 -0500

    Corrections 1

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -16,6 +16,7 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -30,7 +31,8 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+      # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -43,7 +45,9 @@ class CarModel(models.Model):
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
+    
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
